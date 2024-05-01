df985b7ebd1e869a9b1a351b1a4d6e428b3ee881
1hyung <ryu1hyung@naver.com>
2024-05-02 00:27:26 +0900

    study: 부모클래스(Bird)에서 생성자를 활용하는 경우에 자식에서 객체 생성시 전달

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ package org.example
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
 fun main() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
@@ -13,4 +14,36 @@ fun main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
     }
+}*/
+fun main() {
+    var bird = Bird("새")
+    var chicken = Chicken("닭")
+    var sparrow = Sparrow("참새")
+    var pigeon = Pigeon("비둘기")
+
+    bird.fly()
+    chicken.fly()
+    sparrow.fly()
+    pigeon.fly()
+}
+
+open class Bird(
+    var name: String
+) {
+    fun fly() {
+        println("${name} 날아요~")
+    }
+
+}
+
+class Chicken(name: String) : Bird(name) {
+
+}
+
+class Sparrow(name: String) : Bird(name) {
+
+}
+
+class Pigeon(name: String) : Bird(name) {
+
 }
