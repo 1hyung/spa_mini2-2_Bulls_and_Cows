b9bc1c56d490f039468709f638ed18bacdaddf02
1hyung <ryu1hyung@naver.com>
2024-05-02 08:44:42 +0900

    Starcraft 과제 수정

---
+++ src/main/kotlin/StarCraft.kr.kt
@@ -0,0 +1,59 @@
+package org.example
+
+/*fun main() {
+    var unit = Unit("유닛")
+    var zealot = Zealot("질럿")
+    var zergling = Zergling("저글링")
+    var marin = Marin("마린")
+
+    var Fenix = Zealot(100, 14, 14, 6)
+    var DevouringOne = Zergling(70, 6, 6, 38)
+    var JimRaynor = Marin(100, 10, 10, 10)
+
+
+    unit.attack()
+    marin.attack()
+    zergling.attack()
+    zealot.attack()
+
+    unit.hold()
+    marin.hold()
+    zergling.hold()
+    zealot.hold()
+
+    unit.heal(defensivePower : String)
+    marin.heal(defensivePower : String)
+    zergling.heal(defensivePower : String)
+    zealot.heal(defensivePower : String)
+
+    unit.test(something: String)
+    marin.test(something: String)
+    zergling.test(something: String)
+    zealot.test(something: String)
+}*/
+fun main() {
+    val unit = Unit("유닛")
+    val zealot = Zealot("질럿")
+    val zergling = Zergling("저글링")
+    val marin = Marin("마린")
+
+    unit.attack()
+    marin.attack()
+    zergling.attack()
+    zealot.attack()
+
+    unit.hold()
+    marin.hold()
+    zergling.hold()
+    zealot.hold()
+
+    unit.heal("somePower") // defensivePower에 문자열 전달
+    marin.heal("somePower") // defensivePower에 문자열 전달
+    zergling.heal("somePower") // defensivePower에 문자열 전달
+    zealot.heal("somePower") // defensivePower에 문자열 전달
+
+    unit.test("someString")
+    marin.test("someString")
+    zergling.test("someString")
+    zealot.test("someString")
+}
---
+++ src/main/kotlin/Unit.kt
@@ -0,0 +1,86 @@
+package org.example
+
+/*//객체로 이루어진 스타크래프트
+open class Unit(
+    var name: String
+) {
+    fun attack() {
+        println("${name} 공격!")
+        return 5
+    }
+
+    fun hold()
+
+    fun heal(defensivePower : String) {
+        println("체력 회복: ${defensivePower}")
+    }
+
+    fun test(something: String): Int {
+        println("입력받은 값:${something}")
+        return 5
+    }
+}
+
+    class Marin(name: String): Unit(name) {
+        var healthPoints = 35
+        var aggressiveStrength = 5
+        var defensivePower = 5
+        var movingSpeed = 5
+    }
+
+
+    class Zealot(name: String): Unit(name) {
+        var healthPoints = 33
+        var aggressiveStrength = 7
+        var defensivePower = 7
+        var movingSpeed = 3
+    }
+
+    class Zergling(name: String): Unit(name) {
+        var healthPoints = 25
+        var aggressiveStrength = 3
+        var defensivePower = 3
+        var movingSpeed = 19
+    }*/
+
+open class Unit(
+    var name: String
+) {
+    fun attack() {
+        println("$name 공격!")
+    }
+
+    open fun hold() {
+        println("$name 유닛이 대기 중입니다.")
+    }
+
+    open fun heal(defensivePower: String) {
+        println("체력 회복: $defensivePower")
+    }
+
+    open fun test(something: String): Int {
+        println("입력받은 값: $something")
+        return 5
+    }
+}
+
+class Marin(name: String) : Unit(name) {
+    var healthPoints = 35
+    var aggressiveStrength = 5
+    var defensivePower = 5
+    var movingSpeed = 5
+}
+
+class Zealot(name: String) : Unit(name) {
+    var healthPoints = 33
+    var aggressiveStrength = 7
+    var defensivePower = 7
+    var movingSpeed = 3
+}
+
+class Zergling(name: String) : Unit(name) {
+    var healthPoints = 25
+    var aggressiveStrength = 3
+    var defensivePower = 3
+    var movingSpeed = 19
+}
