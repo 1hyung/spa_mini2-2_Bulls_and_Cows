aae8c2d07d0876bac3462d4456586e8760bd1a46
1hyung <ryu1hyung@naver.com>
2024-05-03 00:49:22 +0900

    Feat: <숫자야구2> 순서대로 입력값이 세 자리가 아닐 경우, 숫자로만 이루어져 있지 않을 경우, 중복된 숫자가 있는지 확인하고 아니라면 유효하지 않으므로 'false'를 반환합니다. 모든 유효성을 통과했으면 'true'를 반환한다.

--- src/main/kotlin/Bulls_and_Cows.kt
+++ src/main/kotlin/Bulls_and_Cows.kt
@@ -4,4 +4,11 @@ fun main() {
         numbers.shuffle()
         return numbers.take(3).joinToString(" ")
     }
+
+    fun isInputValid(input: String): Boolean {
+        if (input.length != 3) return false
+        if (!input.all { it.isDigit() }) return false
+        if (input.toSet().size != 3) return false
+        return true
+    }
 }
